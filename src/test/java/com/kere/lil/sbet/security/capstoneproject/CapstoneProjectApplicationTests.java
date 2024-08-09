b7a34b2a89a650260fdb245ec21f0e1cfb2e43f4
Kere <ninokereselidze30@gmail.com>
2024-08-10 00:07:19 +0400

    Commit N1

---
+++ src/test/java/com/kere/lil/sbet/security/capstoneproject/CapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kere.lil.sbet.security.capstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
