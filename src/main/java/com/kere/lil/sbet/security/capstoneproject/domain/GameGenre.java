e9c64594dc0457df3ff1a5c856fd8eaa31961ce4
Kere <ninokereselidze30@gmail.com>
2024-08-10 05:35:00 +0400

    Commit N1

---
+++ src/main/java/com/kere/lil/sbet/security/capstoneproject/domain/GameGenre.java
@@ -0,0 +1,14 @@
+package com.kere.lil.sbet.security.capstoneproject.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class GameGenre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+}
--- src/main/java/com/kere/lil/sbet/security/capstoneproject/domain/User.java
+++ src/main/java/com/kere/lil/sbet/security/capstoneproject/domain/User.java
@@ -1,10 +1,13 @@
 package com.kere.lil.sbet.security.capstoneproject.domain;
 
 
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
-import java.util.Date;
 
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,5 +17,4 @@ public class User {
     private String password;
     private String profilePicture;
     private String fullName;
-    // Getters and Setters
 }
